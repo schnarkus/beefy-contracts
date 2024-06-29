599ea95ec190494e139824bc76518bc14fda32e5
schnarkus <markus.simianer@yahoo.de>
2024-06-29 17:10:15 +0200

    upgrades

--- scripts/vault/archive/balancer/aura arb/upgrade/wsteth.js
+++ scripts/vault/archive/balancer/aura arb/upgrade/wsteth.js
@@ -11,15 +11,14 @@ const {
     AURA: { address: AURA },
     ETH: { address: ETH },
     ARB: { address: ARB },
-    arbUSDCe: { address: arbUSDCe },
   },
 } = addressBook.arbitrum;
 
-const want = web3.utils.toChecksumAddress("0x423A1323c871aBC9d89EB06855bF5347048Fc4A5");
+const want = web3.utils.toChecksumAddress("0x9791d590788598535278552EEcD4b211bFc790CB");
 
 const vaultParams = {
-  mooName: "Dummy Aura Arb 4Pool",
-  mooSymbol: "dummyAuraArb4Pool",
+  mooName: "Dummy Aura Arb wstETH-WETH",
+  mooSymbol: "dummyAuraArbwstETH-WETH",
   delay: 21600,
 };
 
@@ -28,11 +27,11 @@ const bytes0 = "0x00000000000000000000000000000000000000000000000000000000000000
 const strategyParams = {
   want: want,
   inputIsComposable: true,
-  nativeToInputRoute: [["0x64541216bafffeec8ea535bb71fbc927831d0595000100000000000000000002", 0, 1], ["0x423a1323c871abc9d89eb06855bf5347048fc4a5000000000000000000000496", 1, 2]],
+  nativeToInputRoute: [["0x9791d590788598535278552eecd4b211bfc790cb000000000000000000000498", 0, 1]],
   outputToNativeRoute: [["0xcc65a812ce382ab909a11e434dbf75b34f1cc59d000200000000000000000001", 0, 1]],
   booster: "0x98Ef32edd24e2c92525E59afc4475C1242a30184",
-  pid: 28,
-  nativeToInput: [ETH, arbUSDCe, want],
+  pid: 29,
+  nativeToInput: [ETH, want],
   outputToNative: [BAL, ETH],
   unirouter: balancer.router,
   strategist: process.env.STRATEGIST_ADDRESS,
@@ -42,7 +41,7 @@ const strategyParams = {
   beefyVaultProxy: beefyfinance.vaultFactory,
   strategyImplementation: "0xc373250956214d0E196FEC782344641f27076764",
   extraReward: true,
-  secondExtraReward: false,
+  secondExtraReward: true,
   rewardAssets: [AURA, ETH],
   rewardRoute: [
     ["0x64abeae398961c10cbb50ef359f1db41fc3129ff000200000000000000000526", 0, 1],
